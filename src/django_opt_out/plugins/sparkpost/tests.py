f7d4b48989f6a1483026e7cd2e75f3ba0557d75e
Janusz Skonieczny <git@wooyek.com>
2017-11-22 12:51:56 +0100

    move test beside the code

--- src/django_opt_out/plugins/sparkpost/tests.py
+++ src/django_opt_out/plugins/sparkpost/tests.py
@@ -5,10 +5,9 @@ from django.test import TestCase
 from django.test.utils import override_settings
 from mock import patch
 
+from ...tests.test_views import CaptureSignal
 from django_opt_out.models import OptOut
 from django_opt_out.plugins.sparkpost import signals
-
-from ...tests.test_views import CaptureSignal
 
 
 class SparkPostHookTests(TestCase):
