4dc0c99385849f28b1cd30551048f23f531f0d0e
Janusz Skonieczny <git@wooyek.com>
2017-11-22 12:04:14 +0100

    backend loading concept to throw away, here for backup

--- src/django_opt_out/plugins/__init__.py
+++ src/django_opt_out/plugins/__init__.py
@@ -1,1 +1,17 @@
 # coding=utf-8
+
+import importlib
+
+from django.utils.functional import SimpleLazyObject
+
+backends = SimpleLazyObject(lambda: list(get_backedns()))
+
+
+def get_backedns():
+    from django.conf import settings
+    backends = settings.OPT_OUT_BACKENDS
+    for entry in backends:
+        mod_path, _, cls_name = entry.rpartition('.')
+        module = importlib.import_module(mod_path)
+        cls = getattr(module, cls_name)
+        yield cls(settings)
