4ba63e1d76c441990bf3f396d821a0901916f6fb
Janusz Skonieczny <git@wooyek.com>
2017-11-24 18:29:09 +0100

    base test settings on example_project

--- tests/urls.py
+++ tests/urls.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8
 from __future__ import absolute_import, unicode_literals
 
-from django.conf.urls import include, url
+from django.conf.urls import url
 from django.views.generic import TemplateView
 
-# noinspection PyUnresolvedReferences
-urlpatterns = [
-    url(r'^opt-out/', include('django_opt_out.urls')),
-    url(r'^opt-out/', include('django_opt_out.plugins.sparkpost.urls')),
+from website.urls import urlpatterns
+
+urlpatterns += [
     url(r'^mocked_goodbye/(?P<pk>[\d]+)/(?P<secret>[\w]+)/(?P<email>[^/]+)/', TemplateView.as_view(template_name='nonexistent'), name='mocked_goodbye'),
 ]
