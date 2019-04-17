2d0287e33823c7020b283003ffef18f78853e612
Janusz Skonieczny <git.js@bravelabs.pl>
2019-04-17 19:19:07 +0200

    Bump version: 0.9.4 → 0.9.5

--- example_project/src/website/__init__.py
+++ example_project/src/website/__init__.py
@@ -1,3 +1,3 @@
 __author__ = """Janusz Skonieczny"""
 __email__ = 'js+pypi@bravelabs.pl'
-__version__ = '0.9.4'
+__version__ = '0.9.5'
--- src/django_opt_out/__init__.py
+++ src/django_opt_out/__init__.py
@@ -3,4 +3,4 @@
 
 __author__ = """Janusz Skonieczny"""
 __email__ = 'js+pypi@bravelabs.pl'
-__version__ = '0.9.4'
+__version__ = '0.9.5'
